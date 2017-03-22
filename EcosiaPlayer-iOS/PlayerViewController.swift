ee8a317de36f33b84d3670070174e82f3586016a
Igor Camilo <igor.rcamilo@gmail.com>
2017-03-22 20:50:31 -0300

    Building UI.

---
+++ EcosiaPlayer-iOS/PlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerViewController.swift
+//  EcosiaPlayer
+//
+//  Created by Igor Camilo on 22/03/17.
+//  Copyright © 2017 Igor Camilo. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+  
+  @IBOutlet private var coverImageView: UIImageView!
+  @IBOutlet private var trackNameLabel: UILabel!
+  @IBOutlet private var artistLabel: UILabel!
+  @IBOutlet private var currentTimeLabel: UILabel!
+  @IBOutlet private var totalTimeLabel: UILabel!
+  @IBOutlet private var progressSlider: UISlider!
+  @IBOutlet private var playButton: UIButton!
+  @IBOutlet private var pauseButton: UIButton!
+  
+  @IBAction private func playButtonPressed() {
+    
+  }
+  
+  @IBAction private func pauseButtonPressed() {
+    
+  }
+}
