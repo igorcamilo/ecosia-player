a4007a3f1dc4454406cdac437edc485ee304695a
Igor Camilo <igor.rcamilo@gmail.com>
2017-03-22 18:05:23 -0300

    New target: iOS application.

---
+++ EcosiaPlayer-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EcosiaPlayer-iOS
+//
+//  Created by Igor Camilo on 22/03/17.
+//  Copyright © 2017 Igor Camilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
